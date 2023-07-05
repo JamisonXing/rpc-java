cb5a9f8cea1370171162374a045a62290927dbb0
jamison <2810947907@qq.com>
2023-07-05 11:32:52 +0800

    version 3: version2中的东西

--- src/main/java/com/jamison/version3/client/RPCClient.java
+++ src/main/java/com/jamison/version3/client/RPCClient.java
@@ -1,9 +1,10 @@
 package com.jamison.version3.client;
 
+
+
 import com.jamison.version3.common.RPCRequest;
 import com.jamison.version3.common.RPCResponse;
 
-//共性抽取出来
 public interface RPCClient {
     RPCResponse sendRequest(RPCRequest request);
 }
---
+++ src/main/java/com/jamison/version3/service/BlogService.java
@@ -0,0 +1,8 @@
+package com.jamison.version3.service;
+
+
+import com.jamison.version3.common.Blog;
+
+public interface BlogService {
+    Blog getBlogById(Integer id);
+}
