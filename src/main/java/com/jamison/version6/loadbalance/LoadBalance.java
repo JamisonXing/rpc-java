126d73a956008df3e2c36b78f4683c4fc2747b90
jamison <2810947907@qq.com>
2023-07-17 17:44:48 +0800

    version 5: 添加负载均衡策略

---
+++ src/main/java/com/jamison/version6/loadbalance/LoadBalance.java
@@ -0,0 +1,10 @@
+package com.jamison.version6.loadbalance;
+
+import java.util.List;
+
+/**
+ * 给服务器地址列表，根据不同的负载均衡策略选一个
+ */
+public interface LoadBalance {
+    String balance(List<String> addressList);
+}
